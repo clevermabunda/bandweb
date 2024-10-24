f3e475677f1bdb213336d3f4ce6495506f2190fe
Clever <clevermabunda@gmail.com>
2024-10-24 11:08:52 +0200

    add docstrings to models.py

--- band/theband/models.py
+++ band/theband/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
-# Create your models here.
 class Band(models.Model):
+    """
+    Represents a music band.
+
+    Attributes:
+        band_name (str): The name of the band.
+        number_of_members (int): The number of members in the band.
+        year_formed (date): The year the band was formed.
+        genre (str): The musical genre of the band.
+        about_the_band (str, optional): A description of the band. Can be null.
+    """
+
     band_name = models.CharField(max_length=50)
     number_of_members = models.IntegerField()
     year_formed = models.DateField()
@@ -9,4 +19,10 @@ class Band(models.Model):
     about_the_band = models.CharField(max_length=10000, null=True)
 
     def __str__(self) -> str:
+        """
+        Returns a string representation of the Band instance.
+
+        Returns:
+            str: The name of the band.
+        """
         return self.band_name
