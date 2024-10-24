8fd0ccc3afe59d82a996bcf36fce2d014d797ac2
Clever <clevermabunda@gmail.com>
2024-10-24 11:19:08 +0200

    add docstrings to views.py

--- band/theband/views.py
+++ band/theband/views.py
@@ -2,17 +2,46 @@ from django.shortcuts import render, get_object_or_404
 from .models import Band
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 def home(request):
-    return render(request, 'band/homeband.html')\
-    
+    """
+    Render the home page for the band application.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered home page.
+    """
+    return render(request, 'band/homeband.html')
+
 @login_required(login_url="user_auth:login")
 def band_list(request):
+    """
+    Render a list of all bands.
+
+    This view is protected and requires the user to be logged in.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered page displaying the list of bands.
+    """
     list_of_bands = Band.objects.all()
     context = {'list_of_bands': list_of_bands}
     return render(request, 'band/bandlist.html', context)
 
 def band_detail(request, pk):
+    """
+    Render the details of a specific band.
+
+    Args:
+        request: The HTTP request object.
+        pk (int): The primary key of the band to retrieve.
+
+    Returns:
+        HttpResponse: The rendered page displaying the details of the specified band.
+    """
     band = get_object_or_404(Band, pk=pk)
     context = {'band': band}
     return render(request, 'band/bandDetail.html', context)
