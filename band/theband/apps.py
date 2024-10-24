b9ce35c38e1c6156bd6b426da9dcb2e91a4b21f8
Clever <clevermabunda@gmail.com>
2024-10-24 11:24:35 +0200

    add docstrings to apps.py

--- band/theband/apps.py
+++ band/theband/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
 
-
 class ThebandConfig(AppConfig):
+    """
+    Configuration class for the 'theband' application.
+
+    Attributes:
+        default_auto_field (str): The default field type for auto-generated primary keys.
+        name (str): The name of the application.
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'theband'
